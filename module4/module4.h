a2e8bb9cf4b687eb5707df65a7a31b17e86e7940
dany19991 <dany19991@yahoo.com>
2022-04-07 01:16:34 +0300

    fix commit 2

--- module4/module4.h
+++ module4/module4.h
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 void module4_loop(void);
 void module4_func1(void);
 //edit from ipad
+||||||| parent of bf424f7... added module 4 to master
+=======
+void module4_loop(void);
+void module4_func1(void);
+>>>>>>> bf424f7... added module 4 to master
